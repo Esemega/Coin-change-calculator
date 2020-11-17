b587755e270bee840bff09b7fdd8152bb297acb6
Esemega <sogaral89@gmail.com>
2020-11-17 22:49:32 +0100

    starting files

---
+++ app.js
@@ -0,0 +1,9 @@
+AVAILABLE_CHANGE = [200, 100, 50, 20, 10, 5, 1, 2, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+
+//get input values
+
+//calculate the different to know the change back
+
+//Chnage algorithm
+//1. look for the coin or bill bigger that can be given
+//2. repeat the 1. until the module is 0
