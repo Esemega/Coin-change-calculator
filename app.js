69d0f1510bf184d3f195785667ec96d025e63795
Esemega <sogaral89@gmail.com>
2020-11-22 13:59:13 +0100

    adding element for better DOM manipulation, geting the input value directly from DOM, giving chnage back depending on available change, creating small functions for repeated code

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var AVAILABLE_CHANGE = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
 
+
 //Create an html initial structure with javascript
 var createInitialHtmlStructure = (availableChange, rootElement) => {
   var ul = document.createElement("ul");
-  ul.setAttribute("id", "available-change")
-  
+  ul.setAttribute("id", "available-change-list");
+
   for (i = 0; i < availableChange.length; i++) {
     var li = document.createElement("li");
-    var label = document.createElement("label")
+    var label = document.createElement("label");
     var input = document.createElement("input");
-    var span = document.createElement("span");    
+    var span = document.createElement("span");
 
     var labelText = (availableChange[i] >= 1? (availableChange[i] + "€") : (availableChange[i]*100 + "cent")) + " x "; 
     label.textContent = labelText;
     label.setAttribute("for", availableChange[i]);
 
     input.setAttribute("id", availableChange[i]);
+    input.classList.add("badge");
+    input.value = 100;
 
     span.textContent = "Unidades";
 
@@ -23,59 +26,67 @@ var createInitialHtmlStructure = (availableChange, rootElement) => {
     li.appendChild(input);
     li.appendChild(span);
     ul.appendChild(li);
-    }
+  }
 
   var h2 = document.createElement("h2");
   h2.textContent = "Cambio disponible en caja";
 
   rootElement.appendChild(h2);
   rootElement.appendChild(ul);
-}
+};
 
-var getAvailableChangeInputValue = (availableChange) => {
+var getAvailableChangeInputValue = () => {
+  var inputList = document.getElementsByClassName("badge");
 
   var availableChangeInputArray = [];
 
-  for (i = 0; i < availableChange.length; i++) {
+  for (i = 0; i < inputList.length; i++) {
     var inputArray = [];
-    var input = document.getElementById(availableChange[i]);
+    var input = document.getElementById(inputList[i].id);
     inputArray[0] = input.id;
     inputArray[1] = input.value;
     availableChangeInputArray.push(inputArray);
   }
 
-  console.log(availableChangeInputArray);
-
-
-}
+  return availableChangeInputArray;
+};
 
 var changeAlgorithm = (totalImport, quantityGiven, availableChange) => {
   //calculate the different to know the change back
   var difference = quantityGiven - totalImport;
-      //Change algorithm
-    //1. look for the coin or bill bigger that can be given
-    var changeBack = [];
-    for (const badge of availableChange) {
-      if (difference / badge >= 1) {
-        changeBack.push([badge, Math.trunc(difference / badge)]);
+  //Change algorithm
+  //1. look for the coin or bill bigger that can be given
+  var changeBack = [];
+  for (const badge of availableChange) {
+    if (difference / badge[0] >= 1) {
+      var badgeCount = Math.trunc(difference / badge[0]);
+
+      if (badgeCount > badge[1]) {
+        continue;
+      } else {
+        changeBack.push([badge[0], badgeCount]);
         difference =
           Math.round(
-            (difference - badge * Math.floor(difference / badge)) * 100
+            (difference - badge[0] * Math.floor(difference / badge[0])) * 100
           ) / 100;
       }
-      //2. repeat the 1. until the difference is 0
-      if (difference === 0) break;
     }
-    return changeBack;
-}
+    //2. repeat the 1. until the difference is 0
+    if (difference === 0) break;
+  }
+  if (difference !== 0) {
+    return -1;
+  }
+  return changeBack;
+};
 
 var coinOrBill = (value, quantity) => {
   var coinOrBillText = "";
-  var coinOrBillClass="";
+  var coinOrBillClass = "";
   if (value >= 5) {
     if (quantity === 1) {
       coinOrBillText = "billete";
-      coinOrBillClass = "bill"
+      coinOrBillClass = "bill";
     } else {
       coinOrBillText = "billetes";
       coinOrBillClass = "bills";
@@ -83,41 +94,55 @@ var coinOrBill = (value, quantity) => {
   } else {
     if (quantity === 1) {
       coinOrBillText = "moneda";
-      coinOrBillClass = "coin"
+      coinOrBillClass = "coin";
     } else {
       coinOrBillText = "monedas";
-      coinOrBillClass = "coins"
+      coinOrBillClass = "coins";
     }
   }
   return [coinOrBillText, coinOrBillClass];
-}
+};
 
-var createHtmlChangeBackElements = (changeBackList, totalBack, rootElement ) => {
+var showTotalBack = (rootElement, totalBack) => {
+  var h3 = document.createElement("h3");
+  h3.textContent = "Total a devolver: " + totalBack.toFixed(2) + " €";
+
+  rootElement.innerHTML = "";
+  rootElement.appendChild(h3);
+};
+
+var showErrorMessage = (errorMessage, rootElement, deleteRootElemenContent) => {
+  var h2 = document.createElement("h2");
+
+  h2.setAttribute("id", "error-message");
+  h2.textContent = errorMessage;
+
+  if (deleteRootElemenContent) rootElement.innerHTML = "";
+  rootElement.appendChild(h2);
+};
+
+var createHtmlChangeBackElements = (changeBackList, totalBack, rootElement) => {
   var ul = document.createElement("ul");
 
-    for (i = 0; i < changeBackList.length; i++) {
-      var li = document.createElement("li");
-      var span = document.createElement("span");    
+  for (i = 0; i < changeBackList.length; i++) {
+    var li = document.createElement("li");
+    var span = document.createElement("span");
 
-      var coinOrBillArray = coinOrBill(changeBackList[i][0], changeBackList[i][1])
-      
-      span.setAttribute("class",coinOrBillArray[1])
+    var coinOrBillArray = coinOrBill(changeBackList[i][0], changeBackList[i][1])
 
-      span.textContent =
-        changeBackList[i][1] + " " + coinOrBillArray[0] + " de " + changeBackList[i][0].toFixed(2) + "€";
+    span.setAttribute("class", coinOrBillArray[1]);
 
-      li.appendChild(span);
-      ul.appendChild(li);
-    }
+    span.textContent = changeBackList[i][1] + " " + coinOrBillArray[0] +
+      " de " + Number(changeBackList[i][0]).toFixed(2) + "€";
 
-    var h3 = document.createElement("h3");
-    h3.textContent = "Total a devolver: " + totalBack.toFixed(2) + " €";
+    li.appendChild(span);
+    ul.appendChild(li);
+  }
 
-    
-    rootElement.innerHTML = "";
-    rootElement.appendChild(h3);
-    rootElement.appendChild(ul);
-}
+  showTotalBack(rootElement, totalBack);
+
+  rootElement.appendChild(ul);
+};
 
 var buttonEventHandler = () => {
   //get input values
@@ -130,24 +155,35 @@ var buttonEventHandler = () => {
   //get html result div
   var result = document.getElementById("result");
 
+  var errorMessage = "";
+
   if (initialdifference <= 0) {
-    var h2 = document.createElement("h2");
-    h2.setAttribute("id", "error-message")
-    h2.textContent =
-      "La cantidad entregada debe ser mayor que el importe total.";
+    errorMessage = "La cantidad entregada debe ser mayor que el importe total.";
 
-    result.innerHTML = "";
-    result.appendChild(h2);
+    showErrorMessage(errorMessage, result, true);
   } else {
+    var availableChangeInput = getAvailableChangeInputValue();
+    var changeBack = changeAlgorithm(
+      totalImport,
+      quantityGiven,
+      availableChangeInput
+    );
 
-    var changeBack = changeAlgorithm(totalImport, quantityGiven, AVAILABLE_CHANGE);
-    createHtmlChangeBackElements(changeBack, initialdifference, result);
-    
+    if (changeBack === -1) {
+      showTotalBack(result, initialdifference);
+
+      errorMessage =
+        "No hay suficientes monedas o billetes para devolver el cambio.";
+
+      showErrorMessage(errorMessage, result, false);
+    } else {
+      createHtmlChangeBackElements(changeBack, initialdifference, result);
+    }
   }
-
-  getAvailableChangeInputValue(AVAILABLE_CHANGE);
-
 };
+
+document.getElementById("import").value = 123.56;
+document.getElementById("quantity-given").value = 130;
 
 //Event handler for button
 document.getElementById("calculate").addEventListener("click", buttonEventHandler);
